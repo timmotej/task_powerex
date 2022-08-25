42798fe591b9b34e87dc63d14f1c51f81e187dc5
Iga Stribula <iga.stribula@gmail.com>
2022-08-25 22:11:57 +0200

    update lambda function: add env vars and defaults

--- lambda/app/lambda_function.py
+++ lambda/app/lambda_function.py
@@ -1,54 +1,57 @@
 import json
 import re
 import boto3
+import os
 
 import urllib.parse
 
 print('Loading function')
 
 s3 = boto3.client('s3')
+source_bucket = os.getenv("SOURCE_BUCKET","bucket-powerex-files-input")
+target_bucket = os.getenv("TARGET_BUCKET","bucket-powerex-files-output")
+prefix = os.getenv("PREFIX","powerex_")
 
 
-
-def return_filename_with_prefix(name, prefix):
-    """
-    checks if filename and prefix is correct
-    If yes, returns filename with prefix (doesn't duplicate prefices!)
-    If no, returns False
-    """
-    pattern = r"[A-Za-z0-9_.][A-Za-z0-9_.-]*$"
-    if not (re.match(pattern,prefix) and re.match(pattern,name)):
-        return False
-    if prefix in [""] or name.startswith(prefix):
-        return name
-    else:
-        return f"{prefix}{name}"
+# if need not to duplicate prefix
+#def return_filename_with_prefix(name, prefix):
+#    """
+#    checks if filename and prefix is correct
+#    If yes, returns filename with prefix (doesn't duplicate prefices!)
+#    If no, returns False
+#    """
+#    pattern = r"[A-Za-z0-9_.][A-Za-z0-9_.-]*$"
+#    if not (re.match(pattern,prefix) and re.match(pattern,name)):
+#        return False
+#    if prefix in [""] or name.startswith(prefix):
+#        return name
+#    else:
+#        return f"{prefix}{name}"
     
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    source_event_bucket = event['Records'][0]['s3']['bucket']['name']
+    if source_event_bucket != source_bucket:
+        return
+    object_key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    target_bucket = target_bucket
+    copy_source = {'Bucket': source_bucket, 'Key': object_key}
+    
+    print ("Source bucket : ", source_bucket)
+    print ("Target bucket : ", target_bucket)
+    print ("Log Stream name: ", context.log_stream_name)
+    print ("Log Group name: ", context.log_group_name)
+    print ("Request ID: ", context.aws_request_id)
+    print ("Mem. limits(MB): ", context.memory_limit_in_mb)
+    
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
+        print ("Using waiter to waiting for object to persist through s3 service")
+        waiter = s3.get_waiter('object_exists')
+        waiter.wait(Bucket=source_bucket, Key=object_key)
+        s3.copy_object(Bucket=target_bucket, Key=f"{prefix}{object_key}", CopySource=copy_source)
+        s3.Object(copy_source).delete()
         return response['ContentType']
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
-    old_filename, prefix = event["filename"], event["prefix"]
-    # prefix name
-    new_filename = return_filename_with_prefix(old_filename,prefix)
-    if new_filename:
-        return {
-            'statusCode': 200,
-            'body': json.dumps({"new_filename": new_filename, "old_filename": old_filename, "prefix": prefix, "event": event})
-        }
-    else:
-        return {
-            'statusCode': 404,
-            'body': 'Error: Filename or prefix not defined properly'
-        }
+    except Exception as err:
+        print ("Error -"+str(err))
+        return e
+
